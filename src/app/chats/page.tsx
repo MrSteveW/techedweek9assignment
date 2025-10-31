499e86cdff669419270805002e5029e38c200fe4
MrSteveW <MrSteveW@users.noreply.github.com>
2025-10-31 11:32:03 +0000

    styled all chats page

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -2,13 +2,6 @@
 import { db } from "@/utils/connect";
 import Link from "next/link";
 
-type paramsType = {
-  searchParams: {
-    query: string;
-    sort: string;
-  };
-};
-
 export default async function AllChats() {
   const chats = (
     await db.query(`SELECT chats.id,
@@ -22,22 +15,27 @@ export default async function AllChats() {
   return (
     <div className="h-screen">
       <div className="flex justify-center m-4">
-        <div className="w-1/4 text-2xl bg-chat-dark rounded-2xl p-4 text-center">
-          <Link href="/chats/add" className="m-4  hover:text-sliced-cyan">
+        <Link href="/chats/add" className="hover:text-white">
+          <div className="text-2xl bg-chat-dark rounded-2xl px-10 py-2 text-center bg-brew-orange ">
             Add new chat
-          </Link>
-        </div>
-      </div>
-      <div className="flex flex-wrap">
-        {chats.map((chat) => (
-          <div key={chat.id} className="w-1/4 m-4">
-            <Link href={`/chats/${chat.id}`}>
-              <p>{chat.title}</p>
-              <p>{chat.content}</p>
-              <p>{chat.username}</p>
-            </Link>
           </div>
-        ))}
+        </Link>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="w-3/5 bg-white">
+          {chats.map((chat) => (
+            <Link href={`/chats/${chat.id}`} key={chat.id}>
+              <div className="border border-gray-300 flex m-4 p-2">
+                <div className="w-1/5">{chat.username}</div>
+                <div className="w-4/5">
+                  <div className="font-bold">{chat.title}</div>
+                  <div>{chat.content}</div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/ViewChat.tsx
+++ src/components/ViewChat.tsx
@@ -1,5 +1,7 @@
 import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
+import Image from "next/image";
+
 type chatType = {
   chat: {
     id: number;
@@ -13,12 +15,23 @@ type chatType = {
 export default async function ViewChat({ chat }: chatType) {
   const user = await currentUser();
   return (
-    <div className="bg-amber-200 flex flex-row w-1/2 p-4 mb-4 rounded-2xl">
+    <div className="bg-amber-200 flex flex-row w-1/2 p-4 m-4 mb-4 rounded-2xl">
       <div>
-        <div className="m-2">{chat.title}</div>
-        <div className="m-2">By {chat.username}</div>
+        <div className="m-2">{chat.username}</div>
+        <div className="w-10 h-10 relative">
+          <Image
+            src="https://api.dicebear.com/9.x/open-peeps/svg?face=smile"
+            alt=""
+            fill={true}
+            unoptimized
+          />
+        </div>
+      </div>
+      <div>
+        <div className="m-2 font-bold">{chat.title}</div>
+
         <div className="m-2">{chat.content}</div>
-        {user?.username == chat?.user_id && (
+        {user?.id == chat?.user_id && (
           <Link
             href={`/chats/${chat.id}/edit`}
             className="italic  rounded-lg p-1.5 bg-sliced-cyan hover:bg-sliced-blue hover:text-white"
